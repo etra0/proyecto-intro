8b94aa2fe7726ee4beb7b577e3994e8d82fbbf34
Adbias Palacios <adbias_7.0@hotmail.com>
2015-12-18 19:07:28 -0300

    Se creo la ventana de pygame

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-import pygame, sys
+import pygame,sys
 from pygame.locals import *
 
-print 'hola mundo'
+def resize(image,size):
+	image=(pygame.image.load(os.path.join('data',image)))
+	return pygame.transform.scale(image,size)
+
+sprites=[]
+sprites.append(resize('koopa1.png',(32,32)))
+sprites.append(resize('koopa2.png',(32,32)))
+pygame.init()
+window=pygame.display.set_mode((512,434))
+while True:
+	#pygame.display.update()
+	for event in pygame.event.get():
+		if event.type==pygame.QUIT:
+			sys.exit()
