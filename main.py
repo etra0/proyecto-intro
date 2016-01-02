47664ef334d73645ef1c4d14ca221dc4dbf5414b
Adbias Palacios <adbias_7.0@hotmail.com>
2016-01-02 16:26:43 -0300

    Nuevos bloques y sonido
    Agregados bloques 'fire' (daño) y 'brick' (solido), fg cambiado a rock
    agregado sonidos, loop y jump, se muestran bugs de colisiones.

--- main.py
+++ main.py
@@ -5,20 +5,23 @@ import threading as t
 from random import randint
 
 FLAG = True
-diccBloques={'a':[]}
+diccBloques={'rock':[],'fire':[],'brick':[]}
 cont=0
 
 for i in xrange(1000):
-	if cont%3==0:
+	if cont%2==0:
 		if randint(0,1):
-			diccBloques['a'].append([32*i,544])
+			diccBloques['brick'].append([32*i,544])
 		cont+=1
 	else:
-		diccBloques['a'].append([32*i,544])
+		diccBloques['brick'].append([32*i,544])
 		cont+=1
 
 for k in xrange(25):
-	diccBloques['a'].append([32*k*3,544-randint(1,3)*32])
+	diccBloques['rock'].append([32*k*3,544-randint(1,3)*32])
+
+for k in xrange(25):
+	diccBloques['fire'].append([32*k*3,544-randint(1,3)*32])
 
 # def controlArduino(jugador):
 
@@ -54,8 +57,9 @@ def moverPantalla(bloques):
 	global velHor,acelHor
 	velHor+=acelHor
 
-	for i in bloques.values()[0]:
-		i[0]-=velHor
+	for i in bloques.values():
+		for k in i:
+			k[0]-=velHor
 
 	return None
 
@@ -80,12 +84,13 @@ def mover(jugador, unidad, moment, sprites):	# WIP
 
 
 #--------------------------------------------------
+
 clock       	= pygame.time.Clock()
 bAcum			= 0							# Posicion acumulada del movimiento horizontal
 Juego 			= 1 						# Variable booleana que determina si el bucle sigue
 unidad 			= 4 						# Velocidad de movimiento
 personaje 		= [] 						# Lista de sprites del personaje
-bloques 		= [] 						# Lista de sprites de los bloques
+bloques 		= dict() 					# Lista de sprites de los bloques
 acelHor			= 0							# Aceleracion Horizontal
 velHor			= 1							# Velocidad Horizontal
 jugador			= {
@@ -108,13 +113,19 @@ personaje.append (resize('koopa1.png', (32, 64)))  										# Texturas hacia la
 personaje.append (resize('koopa2.png', (32, 64)))										# """"
 personaje.append (pygame.transform.flip(resize('koopa1.png', (32, 64)), True, False)) 	# Texturas hacia la izquierda (derecha con flip)
 personaje.append(pygame.transform.flip(resize('koopa2.png', (32, 64)), True, False))	# """"
-bloques.append(resize('fg.png', (32, 32)))												# Texturas del bloque ***
+bloques['rock']=resize('rock.png', (32, 32))											# Texturas del bloque 1 (solido, no dano)
+bloques['fire']=resize('fire1.png', (16, 16))
+bloques['brick']=resize('brick1.png', (32, 32))
+
 #
 #--------------------------------------------------------------------------------------
 
 pygame.init() 								#Inicializar pygame
 window=pygame.display.set_mode((800, 576))	#Crea ventana 800x576
-
+pygame.mixer.music.load("data/loop.mp3")
+pygame.mixer.music.set_volume(0.15)
+jump=pygame.mixer.Sound("data/jump.wav")
+pygame.mixer.music.play(-1)
 
 while Juego:																# Mainloop
 
@@ -129,7 +140,7 @@ while Juego:																# Mainloop
 
 	for i in diccBloques:													# Dibujo de bloques y anadido para comprobar colisiones
 		for bloque in diccBloques[i]:
-			colisiones.append(window.blit(bloques[0],(bloque[0],bloque[1])))
+			colisiones.append(window.blit(bloques[i],(bloque[0],bloque[1])))
 
 	pygame.display.update()													# Actualizar la pantalla
 
@@ -157,35 +168,56 @@ while Juego:																# Mainloop
 		# 	if event.key==K_LEFT:
 		# 		jugador["direccion"] = "N"
 
-	moment=pygame.time.get_ticks()											# Variable independiente para dibujar la animacion del jugador
+	moment = pygame.time.get_ticks()										# Variable independiente para dibujar la animacion del jugador
 	listacolisiones = jugRect.collidelistall(colisiones) 					# Lista con las colisiones que tiene el personaje
+	timeguide=moment/100
+
+	if timeguide%3==0:
+		bloques['fire']=resize('fire2.png', (16, 16))
+	elif timeguide%3==1:
+		bloques['fire']=resize('fire3.png', (16, 16))
+	elif timeguide%3==2:
+		bloques['fire']=resize('fire1.png', (16, 16))
+	if timeguide%4==0:
+		bloques['brick']=resize('brick1.png', (32, 32))
+	elif timeguide%4==1:
+		bloques['brick']=resize('brick2.png', (32, 32))
+	elif timeguide%4==2:
+		bloques['brick']=resize('brick3.png', (32, 32))
+	elif timeguide%4==3:
+		bloques['brick']=resize('brick4.png', (32, 32))
 
 	# Comparacion de colisiones
 	if listacolisiones==[]:													#Si esta en el aire (no colisiones), debe caer (gravedad)
-		jugador['gravedad'] 	+= 1										
-		jugador["posicion"][1]	+= jugador['gravedad']
+		jugador["gravedad"] 	+= 1										
+		jugador["posicion"][1]	+= jugador["gravedad"]
 
 	else:
 		jugador['gravedad'] 	= 0											# No tiene gravedad debido a que colisiona
-		jugador['posicion'][1] 	= (jugador["posicion"][1])/32*32+1 			# Se le sumo uno porque antes rebotaba infinitamente
+		jugador['posicion'][1] 	= (jugador["posicion"][1])/32*32+2 			# Se le sumo uno porque antes rebotaba infinitamente
 
 		if jugador['salto'] == 1:											# Definicion de salto
 			jugador['gravedad'] 	= -12
 			jugador['posicion'][1] 	+= jugador["gravedad"]					# ***
 			jugador['salto'] 		= 0
+			jump.play()
 
 		for bloque in colisiones:
-			x1,y1=jugador['posicion']										# Coordenada x1, y1 del jugador
+			x1, y1 = jugador['posicion']									# Coordenada x1, y1 del jugador
 			x, y, l, a = bloque												# Coordenada x, y, Longitud y altura del bloque
 
 			if x1+l == x and (y < y1 < y+l or y < y1+l < y+l):				# Comparacion de posicion por la derecha
-				jugador['posicion'][0]-=velHor								# Debe retroceder (<-)
+				#if [x,y] in diccBloques['fire']:
+				#	jugador['vidas']-=1
+				#	jugador['posicion']=[400,0]
+				#else:
+				jugador['posicion'][0] -= velHor						# Debe retroceder (<-)
 
 			#if x1==x+a and (y<=y1<=y1+a or y<=y1+a<=y1+a):					# Comparacion de posicion por la izquierda	(WIP)
 			#	jugador['posicion'][0]+=velHor								# Debe avanzar (->)							(WIP)
 
 
-	if jugador['posicion'][0] > 800 or jugador ['posicion'][1]>600:			# Definicion de perder (WIP)
+	if jugador['posicion'][0] > 800 or jugador ['posicion'][1] > 600:		# Definicion de perder (WIP)
 		jugador['posicion'] = [400,0]
 		jugador['gravedad'] = 0
 
